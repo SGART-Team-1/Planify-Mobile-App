e31554202b9f8bd33e642a433d0e51c328329dd8
Alejandro Galán Moreno <alejandro.galan@alu.uclm.es>
2024-12-06 13:51:57 +0100

    Hola

---
+++ Planify-FE-2024-1.0/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.planify.app',
+  appName: 'Planify',
+  webDir: 'dist/planify-fe-2024/browser',
+};
+
+export default config;
