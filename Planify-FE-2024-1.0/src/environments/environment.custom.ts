c2300b7e8a5fde5e37db4732e4a51bdc8e3a60c3
Manuel <manuel.perales@alu.uclm.es>
2024-12-15 16:09:02 +0100

    Interfaz "Aceptar/Rechazar invitación" corregida

--- Planify-FE-2024-1.0/src/environments/environment.custom.ts
+++ Planify-FE-2024-1.0/src/environments/environment.custom.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://192.168.1.129:8443'
+    apiUrl: 'https://localhost:8443'
     //apiUrl: 'https://4.231.100.23:8443'//usado para el despliegue en la maquina virtual de azure
 
   };
